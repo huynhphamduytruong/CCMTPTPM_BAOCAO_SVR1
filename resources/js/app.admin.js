3ab1ee01b9046106764619eaa0a7b7f6ae5b09a9
Trường Huỳnh <25319326+huynhphamduytruong@users.noreply.github.com>
2021-04-23 07:40:59 +0700

    Upload new resources

---
+++ resources/js/app.admin.js
@@ -0,0 +1,14 @@
+require('./bootstrap');
+
+require('@fortawesome/fontawesome-free/js/brands')
+require('@fortawesome/fontawesome-free/js/solid')
+require('@fortawesome/fontawesome-free/js/regular')
+require('@fortawesome/fontawesome-free/js/fontawesome')
+
+require('jquery-ui')
+
+require('@lgaitan/pace-progress')
+
+require('admin-lte');
+
+require('bootstrap-input-spinner/src/input-spinner')
--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,3 @@
 require('./bootstrap');
+
+require('bootstrap-input-spinner/src/input-spinner')
