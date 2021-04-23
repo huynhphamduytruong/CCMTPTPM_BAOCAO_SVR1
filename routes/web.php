ba54dbe7e538d05489fe4a39553d21ae2575c7b7
Trường Huỳnh <25319326+huynhphamduytruong@users.noreply.github.com>
2021-04-23 07:39:26 +0700

    Update routes

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,47 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', [App\Http\Controllers\HomeController::class, 'index']);
+Route::get('/product/{product}', [\App\Http\Controllers\ProductController::class, 'editProduct']);
+
+// Cart
+Route::get('/cart', [\App\Http\Controllers\CartController::class, 'index'])->name('cart');
+Route::post('/cart/add', [\App\Http\Controllers\CartController::class, 'addToCart']);
+Route::post('/cart/update', [\App\Http\Controllers\CartController::class, 'updateCart']);
+Route::post('/cart/delete', [\App\Http\Controllers\CartController::class, 'deleteFromCart']);
+
+Auth::routes();
+
+Route::prefix('user')->name('user.')->group(function() {
+    Route::get('/', [\App\Http\Controllers\UserController::class, 'index'])->name('index');
+});
+
+Route::prefix('admin')->name('admin.')->middleware(['auth', 'user.is_admin'])->group(function () {
+//    Front page
+    Route::get('/', function() {
+        return redirect('/admin/statistics');
+    })->name('index');
+
+    Route::get('/statistics', [App\Http\Controllers\AdminController::class, 'statistics'])->name('statistics');
+
+    Route::prefix('categories')->name('category.')->group(function() {
+        Route::get('/', [\App\Http\Controllers\AdminController::class, 'categories'])->name('index');
+        Route::match(['get', 'post'], '/edit/{category?}', [\App\Http\Controllers\CategoryController::class, 'editCategory'])->name('edit');
+        Route::get('/delete/{category}', [\App\Http\Controllers\CategoryController::class, 'deleteCategory'])->name('delete');
+    });
+    Route::prefix('brands')->name('brand.')->group(function() {
+        Route::get('/', [\App\Http\Controllers\AdminController::class, 'brands'])->name('index');
+        Route::match(['get', 'post'], '/edit/{brand?}', [\App\Http\Controllers\BrandController::class, 'editBrand'])->name('edit');
+        Route::get('/delete/{brand}', [\App\Http\Controllers\BrandController::class, 'deleteBrand'])->name('delete');
+    });
+    Route::prefix('products')->name('product.')->group(function() {
+        Route::get('/', [\App\Http\Controllers\AdminController::class, 'products'])->name('index');
+        Route::match(['get', 'post'], '/edit/{product?}', [\App\Http\Controllers\ProductController::class, 'editProduct'])->name('edit');
+        Route::get('/delete/{product}', [\App\Http\Controllers\ProductController::class, 'deleteProduct'])->name('delete');
+    });
+    Route::prefix('customers')->name('customer.')->group(function() {
+        Route::get('/', [\App\Http\Controllers\AdminController::class, 'customers'])->name('index');
+    });
+
+
 });
